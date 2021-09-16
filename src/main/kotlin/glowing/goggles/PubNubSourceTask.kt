2a5badaa3d4f22562e3392a5621960cf1457b175
Lukasz Klich <klich.lukasz@gmail.com>
2021-09-16 11:37:28 +0200

    Empty implementations

---
+++ src/main/kotlin/glowing/goggles/PubNubSourceTask.kt
@@ -0,0 +1,22 @@
+package glowing.goggles
+
+import org.apache.kafka.connect.source.SourceRecord
+import org.apache.kafka.connect.source.SourceTask
+
+class PubNubSourceTask : SourceTask() {
+    override fun version(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun start(props: MutableMap<String, String>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+
+    override fun poll(): MutableList<SourceRecord> {
+        TODO("Not yet implemented")
+    }
+}
